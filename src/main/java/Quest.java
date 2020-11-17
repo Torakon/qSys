7a312dda8cc205556fd8a9833fe7543d8b75bcd7
Andrew Lee <oliver.lee@hotmail.co.uk>
2020-11-17 20:25:34 +0000

    added core functions to fit initial tests

--- src/main/java/Quest.java
+++ src/main/java/Quest.java
@@ -1,6 +1,39 @@
+import java.util.ArrayList;
+
 public class Quest {
     private String questID;
+    private ArrayList<Stage> stageData = new ArrayList<>();
+    private Status questStatus;
+
+    public enum Status {
+        NOT_ACCEPTED, ACCEPTED, FAILED, COMPLETE
+    }
+
     public Quest(String id) {
         questID = id;
+    }
+
+    //Get Methods
+    public String getID() {
+        return questID;
+    }
+    public Stage getStageByID() {
+        for(Stage n : stageData){
+            if (n.getID().equals(questID)){
+                return n;
+            }
+        }
+        return null;
+    }
+    public Status getStatus(){
+        return questStatus;
+    }
+
+    //Set Methods
+    public void addStage(String stageID){
+        stageData.add(new Stage(stageID));
+    }
+    public void setStatus(Status status) {
+        questStatus = status;
     }
 }
--- src/main/java/QuestSys.java
+++ src/main/java/QuestSys.java
@@ -1,14 +1,42 @@
+
 import java.util.ArrayList;
 
 public class QuestSys {
-    private ArrayList<Quest> questData = new ArrayList<Quest>();
+    private ArrayList<Quest> questData = new ArrayList<>();
 
     public void createQuest(String questID) {
         Quest questObj = new Quest(questID);
         questData.add(questObj);
     }
 
-    public int getListLength(){
+    //Get Methods
+    public Quest getQuestByID(String questID){
+        for(Quest n : questData){
+            if (n.getID().equals(questID)){
+                return n;
+            }
+        }
+        return null;
+    }
+    public ArrayList<Quest> getQuestByStatus(Quest.Status status) {
+        ArrayList<Quest> statusQuests = new ArrayList<>();
+        for(Quest n : questData){
+            if (n.getStatus() == status){
+                statusQuests.add(n);
+            }
+        }
+        return statusQuests;
+    }
+    public Quest getQuestByIndex(int index) { //needed?
+        return questData.get(index);
+    }
+    public int getListLength() {
         return questData.size();
     }
+    public String getQuestID(int index) {
+        return questData.get(index).getID();
+    }
+
+    //Set Methods
+
 }
