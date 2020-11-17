e95474c86f6da0553f2b91785aacf251133f5eb3
Andrew Lee <oliver.lee@hotmail.co.uk>
2020-11-17 20:36:06 +0000

    added to fullfill tests

---
+++ src/main/java/Stage.java
@@ -0,0 +1,14 @@
+public class Stage {
+    private String stageID;
+    public Stage(String id){
+        stageID = id;
+    }
+
+    //Get Methods
+    public String getID(){
+        return stageID;
+    }
+
+
+    //Set Methods
+}
