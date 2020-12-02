a3472f23472fae1ac69636b3bd214c3a7a913724
Andrew Lee <oliver.lee@hotmail.co.uk>
2020-12-02 07:56:23 +0000

    combined setX and setY, switched to Point2D for xy

--- src/main/java/Stage.java
+++ src/main/java/Stage.java
@@ -1,5 +1,11 @@
+import java.awt.geom.Point2D;
+
 public class Stage {
     private String stageID;
+    private boolean stageComplete = false;
+    private Point2D markerXY = new Point2D.Double(0, 0);
+    private int markerRad = 0;
+
     public Stage(String id){
         stageID = id;
     }
@@ -8,7 +14,26 @@ public class Stage {
     public String getID(){
         return stageID;
     }
-
+    public Point2D getXY() { return markerXY; }
+    public int getMarkerRadius() {
+        return markerRad;
+    }
+    public boolean getStatus() {
+        return stageComplete;
+    }
 
     //Set Methods
+    public void setStatus(boolean bStatus){
+        stageComplete = bStatus;
+    }
+    public void setXY(int x, int y) {
+        markerXY.setLocation(x, y);
+    }
+    public void setXY(Point2D XY){
+        markerXY = XY;
+    }
+    public void setMarkerRadius(int rad) {
+        markerRad = rad;
+    }
+    //add Component System for different quest types?
 }
